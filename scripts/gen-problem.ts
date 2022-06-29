8a1c8fbd026eab0ab0d37ff9939165c6e9e2268b
yamachi4416 <yamachi4416@gmail.com>
2022-06-30 08:48:00 +0900

    タイピング問題のJSON化スクリプトのリファクタリング

--- scripts/gen-problem.ts
+++ scripts/gen-problem.ts
@@ -1,143 +1,167 @@
 import path from "node:path";
-import fs from "node:fs";
+import fs from "node:fs/promises";
 import jaChars from "../libs/TypingJapaneseChars";
 import { optparse } from "./lib/util";
+import { a } from "~~/dist/_nuxt/ModalPanel-a279a22b.mjs";
 
-function listJsonFiles(dir: string) {
-  return fs
-    .readdirSync(dir, { withFileTypes: true })
+async function listJsonFiles(dir: string) {
+  const files = await fs.readdir(dir, { withFileTypes: true });
+  return files
     .filter((entry) => entry.isFile() && entry.name.endsWith(".json"))
     .map((entry) => path.resolve(dir, entry.name));
 }
 
-function generateProblemData(dataDir: string, apiDir: string) {
+async function generateProblemData(dataDir: string, apiDir: string) {
   const problemsDist = path.resolve(apiDir, "problems");
-
-  if (fs.existsSync(problemsDist)) {
-    fs.rmSync(problemsDist, { recursive: true });
-  }
-  fs.mkdirSync(problemsDist, { recursive: true });
-
+  const problemsFile = path.join(apiDir, "problems.json");
+  const newProblemsFile = path.join(apiDir, "newProblems.json");
   const tagsFile = path.join(apiDir, "tags.json");
-  const tags = fs.existsSync(tagsFile)
-    ? JSON.parse(String(fs.readFileSync(tagsFile)))
-    : {};
+  const tagsDist = path.resolve(apiDir, "tags");
+
+  await fs.rm(problemsDist, { recursive: true }).catch(() => {});
+  await fs.mkdir(problemsDist, { recursive: true });
+
+  const tags = await fs
+    .readFile(tagsFile, { flag: "r" })
+    .then((buf) => JSON.parse(buf.toString()))
+    .catch(() => ({}));
+
   let tagId = Object.values(tags).length + 1;
   Object.values(tags).forEach((m: any) => {
     m.problems = [];
   });
 
-  const problems = listJsonFiles(dataDir).map((p) => {
-    const dataObj = JSON.parse(String(fs.readFileSync(p)));
-    const problem = {
-      id: path.basename(p, ".json"),
-      ...dataObj,
-    };
-
-    if (problem.type === "japanese") {
-      for (const word of problem.words) {
-        word.word = jaChars.typeJapaneseChars(word.info2);
-      }
-    }
-
-    problem.tags = problem.tags.map((name) => {
-      if (!tags[name]) {
-        tags[name] = {
-          id: ("" + tagId++).padStart(5, "0"),
-          problems: [],
-        };
-      }
-
-      return { id: tags[name].id, name };
-    });
-
-    const dist = path.resolve(problemsDist, path.basename(p));
-    fs.writeFileSync(dist, JSON.stringify(problem, null, 2));
-
-    problem.stat = fs.statSync(p);
-
-    return problem;
-  });
-
-  const summary = {
-    problems: problems.map((p) => {
+  const problems = await Promise.all(
+    (
+      await listJsonFiles(dataDir)
+    ).map(async (p) => {
+      const dataObj = JSON.parse((await fs.readFile(p)).toString());
       const problem = {
-        id: p.id,
-        title: p.title,
-        type: p.type,
-        words: p.words.length,
-        chars: p.words.reduce((s, w) => s + w.word.length, 0),
-        createdAt: p.createdAt,
-        updatedAt: p.updatedAt,
-        tags: p.tags || [],
+        id: path.basename(p, ".json"),
+        ...dataObj,
       };
 
-      problem.tags.forEach((m) => {
-        tags[m.name].problems.push(problem);
+      if (problem.type === "japanese") {
+        for (const word of problem.words) {
+          word.word = jaChars.typeJapaneseChars(word.info2);
+        }
+      }
+
+      problem.tags = problem.tags.map((name) => {
+        if (!tags[name]) {
+          tags[name] = {
+            id: ("" + tagId++).padStart(5, "0"),
+            problems: [],
+          };
+        }
+
+        return { id: tags[name].id, name };
       });
 
-      return problem;
-    }),
-  };
+      const dist = path.resolve(problemsDist, path.basename(p));
+      await fs.writeFile(dist, JSON.stringify(problem, null, 2));
 
-  fs.writeFileSync(
-    path.join(apiDir, "problems.json"),
-    JSON.stringify(summary, null, 2)
+      problem.stat = await fs.stat(p);
+
+      return problem;
+    })
   );
 
-  const tagsDist = path.resolve(apiDir, "tags");
+  await fs.writeFile(
+    problemsFile,
+    JSON.stringify(
+      {
+        problems: problems.map((p) => {
+          const problem = {
+            id: p.id,
+            title: p.title,
+            type: p.type,
+            words: p.words.length,
+            chars: p.words.reduce((s, w) => s + w.word.length, 0),
+            createdAt: p.createdAt,
+            updatedAt: p.updatedAt,
+            tags: p.tags || [],
+          };
 
-  if (fs.existsSync(tagsDist)) {
-    fs.rmSync(tagsDist, { recursive: true });
-  }
-  fs.mkdirSync(tagsDist, { recursive: true });
+          problem.tags.forEach((m) => {
+            tags[m.name].problems.push(problem);
+          });
 
-  const tagSummary = {};
-  for (const tagName of Object.keys(tags)) {
-    const tag = tags[tagName];
-    tagSummary[tagName] = {
-      id: tag.id,
-      count: tag.problems.length,
-    };
-    const tagProblemsFile = path.join(tagsDist, `${tag.id}.json`);
-    fs.writeFileSync(
-      tagProblemsFile,
-      JSON.stringify(
-        {
-          id: tag.id,
-          name: tagName,
-          problems: tag.problems,
-        },
-        null,
-        2
-      )
-    );
-  }
-
-  fs.writeFileSync(tagsFile, JSON.stringify(tagSummary, null, 2));
-
-  const newProblems = Array.from(problems)
-    .sort(
-      (a, b) =>
-        new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime() ||
-        new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime() ||
-        b.stat.birthtimeMs - a.stat.birthtimeMs
+          return problem;
+        }),
+      },
+      null,
+      2
     )
-    .slice(0, 6)
-    .map((p) => ({
-      id: p.id,
-      title: p.title,
-      type: p.type,
-      words: p.words.length,
-      chars: p.words.reduce((s, w) => s + w.word.length, 0),
-      createdAt: p.createdAt,
-      updatedAt: p.updatedAt,
-      tags: p.tags || [],
-    }));
+  );
 
-  fs.writeFileSync(
-    path.join(apiDir, "newProblems.json"),
-    JSON.stringify(newProblems, null, 2)
+  await fs.rm(tagsDist, { recursive: true }).catch(() => {});
+  await fs.mkdir(tagsDist, { recursive: true });
+
+  const tagSummary = await Promise.all(
+    Object.keys(tags).map(async (tagName) => {
+      const tag = tags[tagName];
+      await fs.writeFile(
+        path.join(tagsDist, `${tag.id}.json`),
+        JSON.stringify(
+          {
+            id: tag.id,
+            name: tagName,
+            problems: tag.problems,
+          },
+          null,
+          2
+        )
+      );
+      return {
+        name: tagName,
+        id: tag.id,
+        count: tag.problems.length,
+      };
+    })
+  );
+
+  await fs.writeFile(
+    tagsFile,
+    JSON.stringify(
+      tagSummary
+        .sort((a, b) => (a.id < b.id ? -1 : 1))
+        .reduce(
+          (summary, { name, id, count }) => ({
+            ...summary,
+            [name]: { id, count },
+          }),
+          {}
+        ),
+      null,
+      2
+    )
+  );
+
+  await fs.writeFile(
+    newProblemsFile,
+    JSON.stringify(
+      problems
+        .sort(
+          (a, b) =>
+            new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime() ||
+            new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime() ||
+            b.stat.birthtimeMs - a.stat.birthtimeMs
+        )
+        .slice(0, 6)
+        .map((p) => ({
+          id: p.id,
+          title: p.title,
+          type: p.type,
+          words: p.words.length,
+          chars: p.words.reduce((s, w) => s + w.word.length, 0),
+          createdAt: p.createdAt,
+          updatedAt: p.updatedAt,
+          tags: p.tags || [],
+        })),
+      null,
+      2
+    )
   );
 }
 
@@ -153,7 +177,7 @@ Options:
   );
 }
 
-function main() {
+async function main() {
   const { opts } = optparse();
 
   if ((opts["--help"] ?? opts["-h"]) !== undefined) {
@@ -169,9 +193,9 @@ function main() {
     return 1;
   }
 
-  generateProblemData(input, output);
+  await generateProblemData(input, output);
 
   return 0;
 }
 
-process.exit(main());
+main().then((code) => process.exit(code));
