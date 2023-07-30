c7fdc046aaab4c36e002c19d7238c5f01613329a
yamachi4416 <yamachi4416@gmail.com>
2023-07-30 16:20:37 +0900

    トップページの問題検索のテストコードのリファクタリング

--- test/e2e/index/search.spec.ts
+++ test/e2e/index/search.spec.ts
@@ -1,30 +1,35 @@
-import { describe, expect, it } from 'vitest'
+import { expect, it } from 'vitest'
 import { createPage } from '@nuxt/test-utils'
 import { expectLoadingHidden, waitForRouterPath } from '~~/test/e2e/util'
 
-describe('問題いちらんの確認', () => {
-  it("トップページの'検索'ボタンをクリックすると問題いちらんページに遷移する", async () => {
-    const page = await createPage('/')
+it("トップページの'検索'フォームから問題を検索することができる", async () => {
+  const page = await createPage('/')
 
-    const container = page.getByRole('main')
-    const search = container.getByRole('search')
-    const input = search.getByRole('textbox', {
-      name: '検索キーワード',
-      exact: true,
-    })
-    const button = search.getByRole('button', { name: '検索する', exact: true })
-
-    await input.fill('駅')
-    await button.click()
-
-    await waitForRouterPath(page, '/problems')
-
-    const heading = container.getByRole('heading', {
-      name: '駅 の検索結果',
-      exact: true,
-    })
-
-    expect(await heading.isVisible()).toBeTruthy()
-    await expectLoadingHidden(page)
+  const container = page.getByRole('main')
+  const search = container.getByRole('search')
+  const input = search.getByRole('textbox', {
+    name: '検索キーワード',
+    exact: true,
   })
+  const button = search.getByRole('button', { name: '検索する', exact: true })
+
+  await input.fill('駅')
+  await button.click()
+
+  await waitForRouterPath(page, '/problems')
+
+  const heading = container.getByRole('heading', {
+    name: '駅 の検索結果',
+    exact: true,
+  })
+
+  expect(await heading.isVisible()).toBeTruthy()
+  await expectLoadingHidden(page)
+
+  const titles = await container
+    .getByRole('article')
+    .getByRole('heading', { name: '駅', exact: false })
+    .all()
+
+  expect(titles.length).greaterThan(0)
 })
