f7b4a8e113b8db4ae8f01086ebe0f2dba06c71cd
yamachi4416 <yamachi4416@gmail.com>
2023-07-30 17:44:33 +0900

    ページヘッダーの画面遷移の確認のテストの遷移先間違いの修正

--- test/e2e/index/routes/header.spec.ts
+++ test/e2e/index/routes/header.spec.ts
@@ -8,14 +8,15 @@ import {
 
 describe('ページヘッダーの画面遷移の確認', () => {
   it.each([
-    { name: 'サイトポリシー', path: '/policy', title: 'サイトポリシー' },
-    { name: 'お問い合わせ', path: '/contact', title: 'お問い合わせ' },
+    { name: 'プレイする', path: '/game/menu', title: 'タイピングメニュー' },
+    { name: '問題いちらん', path: '/problems', title: '問題いちらん' },
+    { name: 'サイト説明', path: '/about', title: 'サイト説明' },
   ])(
     'ナビゲーションの$nameリンクをクリックすると$titleページに遷移する',
     async ({ name, path, title }) => {
       const page = await createPage('/')
 
-      const container = page.getByRole('navigation')
+      const container = page.getByRole('banner').getByRole('navigation')
       const link = container.getByRole('link', { name, exact: true })
 
       expect(await link.isVisible()).toBeTruthy()
