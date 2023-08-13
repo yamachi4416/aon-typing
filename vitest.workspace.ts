30ada2f9ddc67901dada02ab8312430569e2cbcd
yamachi4416 <yamachi4416@gmail.com>
2023-08-13 14:59:06 +0900

    パッケージマネージャーをpnpmに変更

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,32 +1,26 @@
-import { defineWorkspace, defineConfig, mergeConfig } from 'vitest/config'
+import { defineWorkspace, mergeConfig, type UserConfig } from 'vitest/config'
 
-const sharedConfig = defineConfig({
+const sharedConfig: UserConfig = {
   resolve: {
     alias: {
       '~': './src',
       '~~': '.',
     },
   },
+}
+
+const unitConfig = mergeConfig<UserConfig, UserConfig>(sharedConfig, {
+  test: {
+    dir: './test/unit',
+  },
 })
 
-const unitConfig = mergeConfig(
-  sharedConfig,
-  defineConfig({
-    test: {
-      dir: './test/unit',
-    },
-  }),
-)
-
-const e2eConfig = mergeConfig(
-  sharedConfig,
-  defineConfig({
-    test: {
-      dir: './test/e2e',
-      globalSetup: ['./test/e2e/setup.ts'],
-      testTimeout: 30_000,
-    },
-  }),
-)
+const e2eConfig = mergeConfig<UserConfig, UserConfig>(sharedConfig, {
+  test: {
+    dir: './test/e2e',
+    globalSetup: ['./test/e2e/setup.ts'],
+    testTimeout: 30_000,
+  },
+})
 
 export default defineWorkspace([unitConfig, e2eConfig])
