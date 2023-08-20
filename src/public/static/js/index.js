5103e08ca8b3419d330b0af9f1ad7af7da7dc1e6
yamachi4416 <yamachi4416@gmail.com>
2023-08-20 22:53:12 +0900

    Prettier実行の対象ファイルの指定を整理

--- src/public/static/js/index.js
+++ src/public/static/js/index.js
@@ -1,9 +1,9 @@
-(function () {
-  const html = document.getElementsByTagName('html')[0];
+;(function () {
+  const html = document.getElementsByTagName('html')[0]
   try {
-    const theme = localStorage.getItem('theme');
+    const theme = localStorage.getItem('theme')
     if (theme === 'dark') {
-      html.classList.add('dark');
+      html.classList.add('dark')
     }
   } catch (e) {}
-})();
+})()
