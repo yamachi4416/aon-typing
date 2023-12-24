813c53f401080068e7027c89dcda47285cc4b808
yamachi4416 <yamachi4416@gmail.com>
2023-12-24 20:21:43 +0900

    タイピングパネルのスタイル指定をscopedからmoduleに変更

--- src/components/mod/game/TypingPanel.vue
+++ src/components/mod/game/TypingPanel.vue
@@ -1,23 +1,25 @@
 <template>
   <svg
-    class="typing-game-panel"
-    :flash-typing-mistake="flashTypingMistake"
-    :caps-lock="keys.isCapsLock"
+    :class="{
+      [$style.panel]: true,
+      [$style['panel-mistake']]: flashTypingMistake,
+      [$style['panel-capslock']]: keys.isCapsLock,
+    }"
     viewBox="0 0 1000 470"
     width="1000"
     height="470"
     xmlns="http://www.w3.org/2000/svg"
   >
     <PartsLineGauge
-      class="line-gauge"
+      :class="$style['line-gauge']"
       :limit="typing.goalCharCount || typing.totalCharCount || 0"
       :used="typing.totalTypeCorrect"
     />
-    <foreignObject class="typing-game" width="1000" height="470">
-      <div class="wrapper">
-        <div class="display-zone">
-          <div class="display-zone-info">
-            <div class="display-zone-info-left">
+    <foreignObject :class="$style.typing" width="1000" height="470">
+      <div>
+        <div :class="$style['typing-display']">
+          <div>
+            <div :class="$style['typing-display-left']">
               <PartsTimeCircle
                 v-if="typing.timeLimit > 0"
                 :total-time="typing.timeLimit"
@@ -35,35 +37,39 @@
                 @click="pauseToggle"
               />
             </div>
-            <div class="display-zone-info-center">
-              <div class="display-words-info" :class="infoClass">
-                <span class="display-words-info-text" v-text="infoState.info" />
+            <div :class="$style['typing-display-center']">
+              <div
+                :class="$style['typing-display-info1']"
+                :data-word-count="wordInfo1.count"
+                :data-word-type="wordInfo1.type"
+              >
+                <span v-text="infoState.info" />
               </div>
               <ModKbdDisplayWords
                 v-if="!!infoState.word"
                 width="100%"
-                class="display-words-info2"
+                :class="$style['typing-display-info2']"
                 :word="infoState"
                 :char-mode="false"
               />
               <ModKbdDisplayWords
                 width="100%"
-                class="display-words-word"
+                :class="$style['typing-display-words']"
                 :word="current.wordState"
               />
             </div>
-            <div class="display-zone-info-right">
-              <div class="close-circle">
+            <div :class="$style['typing-display-right']">
+              <div :class="$style['close-circle']">
                 <PartsCloseCircle tabindex="-1" @click="cancel" />
               </div>
             </div>
           </div>
         </div>
-        <div class="hands">
+        <div :class="$style['typing-hands']">
           <ModKbdHandMap :hand-numbers="handNumbers.L" />
           <ModKbdHandMap :hand-numbers="handNumbers.R" />
         </div>
-        <div class="keyboard-zone">
+        <div :class="$style['typing-keyboard']">
           <ModKbdTypingKeyboard
             v-if="keys"
             :type-key="typeKey"
@@ -112,11 +118,11 @@ const { flash: flashTypingMistake } = useFlashing({
   timeout: 120,
 })
 
-const infoClass = computed(() => {
+const wordInfo1 = computed(() => {
   const chars = infoState.value.info?.length || 0
-  const n = [10, 20, 30, 40, 50, 100, 200, 300].find((c) => chars <= c) || 0
-  const type = problem.value.type ?? 'unknown'
-  return { [`chars-${n}`]: true, [type]: true }
+  const count = [10, 20, 30, 40, 50, 100, 200, 300].find((c) => chars <= c) || 0
+  const type = problem.value.type
+  return { count, type }
 })
 
 onBeforeUnmount(() => props.typing.dispose())
@@ -138,206 +144,146 @@ function orDefaultComputed<T>(source: () => T, defaultValue: any) {
 }
 </script>
 
-<style lang="scss" scoped>
-.typing-game-panel {
-  pointer-events: none;
+<style lang="scss" module>
+@use 'sass:list';
 
-  &[flash-typing-mistake='true'] {
-    --color-p: var(--input-error-message);
-    --keyboard-highlight: var(--input-error-message);
-  }
+.panel {
+  pointer-events: none;
 
   --keyboard-capslock-stroke: transparent;
   --keyboard-capslock-fill: transparent;
-
-  &[caps-lock='true'] {
-    --keyboard-capslock-stroke: var(--color-3);
-    --keyboard-capslock-fill: var(--keyboard-highlight);
-  }
 
   background: var(--background-60);
   border: 2px solid var(--color-3);
   border-radius: 10px;
   box-shadow: var(--shadow-color-lg) 3px 3px 9px;
+}
 
-  .line-gauge {
-    transform: translate(-1px, -1px);
-  }
+.panel-mistake {
+  --color-p: var(--input-error-message);
+  --keyboard-highlight: var(--input-error-message);
+}
 
-  .typing-game {
+.panel-capslock {
+  --keyboard-capslock-stroke: var(--color-3);
+  --keyboard-capslock-fill: var(--keyboard-highlight);
+}
+
+.line-gauge {
+  transform: translate(-1px, -1px);
+}
+
+.typing {
+  width: 100%;
+  height: 100%;
+  max-height: 100%;
+  overflow: hidden;
+
+  & > * {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     width: 100%;
     height: 100%;
-    max-height: 100%;
-    overflow: hidden;
+    padding: 15px;
+  }
+}
 
-    .wrapper {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      width: 100%;
-      height: 100%;
-      padding: 15px;
-    }
+.typing-display {
+  width: 100%;
 
-    .display-zone {
-      width: 100%;
+  & > * {
+    display: flex;
+    align-items: flex-start;
+  }
+}
 
-      &-info {
-        display: flex;
-        align-items: flex-start;
+.typing-display-left {
+  width: 60px;
+}
 
-        &-left,
-        &-right {
-          width: 60px;
-        }
+.typing-display-center {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  min-height: 100px;
+  overflow: hidden;
 
-        &-right {
-          display: flex;
-          justify-content: flex-end;
+  & > * {
+    width: 100%;
+  }
+}
 
-          .close-circle {
-            width: 35px;
-            height: 35px;
-            margin-top: -5px;
-            margin-right: -5px;
-          }
-        }
+.typing-display-right {
+  display: flex;
+  justify-content: flex-end;
+  width: 60px;
+}
 
-        &-center {
-          display: flex;
-          flex: 1;
-          flex-direction: column;
-          min-height: 100px;
-          overflow: hidden;
+.typing-display-info1 {
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  font-size: 1em;
+  line-height: 1.2;
+  color: var(--color-6);
+  white-space: pre-wrap;
 
-          & > * {
-            width: 100%;
-          }
+  & > * {
+    white-space: pre-wrap;
+  }
 
-          .display-words-info {
-            display: flex;
-            flex: 1;
-            align-items: center;
-            justify-content: center;
-            font-size: 1em;
-            line-height: 1.2;
-            color: var(--color-6);
+  &[data-word-type='english'] {
+    font-size: 1.2em;
+    line-height: 1.4;
+  }
 
-            &-text {
-              white-space: pre-wrap;
-            }
+  $word-counts: 10, 20, 30, 40, 50, 100, 200, 300;
 
-            &.english {
-              font-size: 1.2em;
-              line-height: 1.4;
-            }
+  @each $count in $word-counts {
+    $index: list.index($word-counts, $count);
+    $size-diff: ($index - 1) * 0.1em;
 
-            &.chars-10 {
-              font-size: 1.5em;
+    &[data-word-count='#{$count}'] {
+      font-size: 1.5em - $size-diff;
 
-              &.english {
-                font-size: 1.6em;
-              }
-            }
-
-            &.chars-20 {
-              font-size: 1.4em;
-
-              &.english {
-                font-size: 1.5em;
-              }
-            }
-
-            &.chars-30 {
-              font-size: 1.3em;
-
-              &.english {
-                font-size: 1.4em;
-              }
-            }
-
-            &.chars-40 {
-              font-size: 1.2em;
-
-              &.english {
-                font-size: 1.3em;
-              }
-            }
-
-            &.chars-50 {
-              font-size: 1.1em;
-
-              &.english {
-                font-size: 1.2em;
-              }
-            }
-
-            &.chars-200 {
-              font-size: 0.9em;
-
-              &.english {
-                font-size: 1em;
-              }
-            }
-
-            &.chars-300 {
-              font-size: 0.8em;
-
-              &.english {
-                font-size: 0.9em;
-              }
-            }
-          }
-
-          .display-words-info2 {
-            font-size: 1.1em;
-            letter-spacing: 0.1em;
-          }
-
-          .display-words-word {
-            font-size: 1.5em;
-            letter-spacing: 0.15em;
-          }
-        }
-      }
-
-      &-words {
-        width: 90%;
-        margin: 0 auto;
-
-        .display-words {
-          width: 100%;
-        }
-      }
-    }
-
-    .words-count {
-      display: flex;
-      margin: 0;
-      font-size: 14px;
-      color: var(--color-6);
-      white-space: nowrap;
-
-      &-types-count {
-        width: 3em;
-        text-align: right;
-      }
-    }
-
-    .keyboard-zone {
-      width: 98%;
-    }
-
-    .hands {
-      display: flex;
-      gap: 20px 20px;
-      width: 90%;
-      height: 15px;
-
-      & > * {
-        width: 100%;
+      &[data-word-type='english'] {
+        font-size: 1.6em - $size-diff;
       }
     }
   }
+}
+
+.typing-display-info2 {
+  font-size: 1.1em;
+  letter-spacing: 0.1em;
+}
+
+.typing-display-words {
+  font-size: 1.5em;
+  letter-spacing: 0.15em;
+}
+
+.close-circle {
+  width: 35px;
+  height: 35px;
+  margin-top: -5px;
+  margin-right: -5px;
+}
+
+.typing-hands {
+  display: flex;
+  gap: 20px 20px;
+  width: 90%;
+  height: 15px;
+
+  & > * {
+    width: 100%;
+  }
+}
+
+.typing-keyboard {
+  width: 98%;
 }
 </style>
