4e7bdf5c9d2270395e66f7e162145f69eee31d52
yamachi4416 <yamachi4416@gmail.com>
2026-02-11 08:05:19 +0900

    setGlobalHeadを使用してGoogle Fontsのリンクを設定

--- modules/googleFonts.ts
+++ modules/googleFonts.ts
@@ -1,26 +1,29 @@
-import { defineNuxtModule } from '@nuxt/kit'
-import { constructURL } from 'google-fonts-helper'
+import { defineNuxtModule, setGlobalHead } from '@nuxt/kit'
 import type { GoogleFonts } from 'google-fonts-helper'
+import { constructURL } from 'google-fonts-helper'
 
 export default defineNuxtModule<GoogleFonts>({
   meta: {
     name: 'googleFonts',
   },
-  setup(options, nuxt) {
+  setup(options, _nuxt) {
     const googleFontUrl = constructURL(options)
     if (!googleFontUrl) return
 
-    nuxt.options.app.head.link ??= []
-    nuxt.options.app.head.link.push({
-      rel: 'preconnect',
-      href: 'https://fonts.googleapis.com',
-    }, {
-      rel: 'preconnect',
-      href: 'https://fonts.gstatic.com',
-      crossorigin: 'anonymous',
-    }, {
-      rel: 'stylesheet',
-      href: googleFontUrl,
+    setGlobalHead({
+      link: [
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com',
+        }, {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: 'anonymous',
+        }, {
+          rel: 'stylesheet',
+          href: googleFontUrl,
+        },
+      ],
     })
   },
 })
