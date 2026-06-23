5bc1f37886cf4883351fb9b78eb8abb06e6f536c
yamachi4416 <yamachi4416@gmail.com>
2026-06-23 22:54:27 +0900

    Vitest Config の調整

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,49 +1,31 @@
-import { defineVitestProject } from '@nuxt/test-utils/config'
-import type { ViteUserConfig as UserConfig } from 'vitest/config'
 import { defineConfig, mergeConfig } from 'vitest/config'
+import { defineVitestProject } from '@nuxt/test-utils/config'
 import { playwright } from '@vitest/browser-playwright'
-
-const reolveAliasConfig: UserConfig = {
-  resolve: {
-    alias: {
-      '~': new URL('./app', import.meta.url).pathname,
-      '~~': new URL('./', import.meta.url).pathname,
-    },
-  },
-  test: {
-    globals: true,
-  },
-}
 
 export default defineConfig({
   test: {
     coverage: {
       exclude: ['./test/**/*', './app/assets/**/*'],
     },
-    hookTimeout: 30_000,
-    testTimeout: 30_000,
-    passWithNoTests: true,
     projects: [
-      mergeConfig(reolveAliasConfig, {
+      {
         test: {
           name: 'unit',
           dir: './test/unit',
           environment: 'happy-dom',
         },
-      } satisfies UserConfig),
-      defineVitestProject({
+      },
+      await defineVitestProject({
         test: {
           name: 'app:nuxt',
           dir: './test/app',
-          globals: true,
         },
       }),
-      defineVitestProject({
+      await defineVitestProject({
         test: {
           name: 'app:browser',
           dir: './test/browser',
           environment: 'nuxt',
-          globals: true,
           browser: {
             enabled: true,
             screenshotFailures: false,
@@ -52,15 +34,27 @@ export default defineConfig({
           },
         },
       }),
-      mergeConfig(reolveAliasConfig, {
+      {
         test: {
           name: 'e2e',
           dir: './test/e2e',
           globalSetup: ['./test/e2e/globalSetup.ts'],
           setupFiles: ['./test/e2e/setup.ts'],
         },
-      } satisfies UserConfig),
-    ],
+      },
+    ].map((config) => mergeConfig({
+      resolve: {
+        alias: {
+          '~': new URL('./app', import.meta.url).pathname,
+          '~~': new URL('./', import.meta.url).pathname,
+        },
+      },
+      test: {
+        globals: true,
+        hookTimeout: 30_000,
+        testTimeout: 30_000,
+      },
+    } satisfies typeof config, config)),
     onConsoleLog(log) {
       if (log.includes('<Suspense> is an experimental feature')) {
         return false
