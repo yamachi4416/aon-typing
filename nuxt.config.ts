67c483826f708555bff7410cdfae68e29e913e5d
yamachi4416 <yamachi4416@gmail.com>
2026-09-06 15:55:40 +0900

    validateAppHead に従って app.head の不要な設定を削除

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -55,8 +55,6 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
       htmlAttrs: {
         lang: 'ja',
       },
